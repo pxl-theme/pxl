9de502b4160b7ca280214e56801cf1636cedc781
Ege Esin <aegean@duck.com>
2024-02-24 00:42:43 +0300

    feat: Add share links from Mastodon, Twitter - Add Mastodon (with instance prompt), Twitter share links

--- static/script/main.js
+++ static/script/main.js
@@ -443,51 +443,8 @@ if (navRespo) {
 })();
 
 //--- Pagination (for Eleventy)
-// Source:
+// Based on:
 // https://github.com/eduardoboucas/buildtimes/blob/master/assets/js/main.js
-
-// --------------------------------------------------------------
-// UI events
-// --------------------------------------------------------------
-
-function bindUIEvents() {
-	window.addEventListener("load", () => {
-		// Loading content from previous pages
-		if (loopContainer?.dataset.paginatorCurrent > 1) {
-			const firstPost = document.querySelector(".js-loop__item");
-			const initialOffset = firstPost.offsetTop;
-
-			loadPosts(() => {
-				const diff = firstPost.offsetTop - initialOffset;
-
-				reloadThings();
-
-				document.documentElement.scrollTop = diff;
-			}, true);
-		}
-	});
-
-	loadMoreButton?.addEventListener("click", () => {
-		// this.classList.add("._spinner");
-		loadMoreButton.insertAdjacentHTML(
-			"beforeend",
-			"<div class='_spinner'></div>",
-		);
-		loadPosts(() => {
-			reloadThings();
-			if (loadMoreButton.querySelector("._spinner"))
-				loadMoreButton.querySelector("._spinner").remove();
-		});
-		debugMode && console.log("[pxl] Posts loaded");
-		// sendGAEvent("Posts", "Load more");
-		return false;
-	});
-
-	// document.querySelector(".js-loop__item-navigation-arrow").addEventListener("click", () => {
-	//     sendGAEvent("Posts", "Post navigation");
-	// });
-}
-
 function fetchPages(pageNumbers, callback) {
 	const pages = [];
 
@@ -621,10 +578,111 @@ function loadPosts(callback, previousPages) {
 // 	}
 // }
 
-// --------------------------------------------------------------
-// Initialisation
-// --------------------------------------------------------------
+//--- Mastodon Instance Prompt
+// Based on:
+// https://github.com/clagnut/Clagnut.com/blob/master/js/global.js
 
-bindUIEvents();
+function createCookie(name, value, days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		var expires = `; expires=${date.toGMTString()};`;
+	} else expires = "";
+	document.cookie = `${name}=${value}${expires}; path=/`;
+}
+
+function readCookie(name) {
+	var nameEQ = `${name}=`;
+	var ca = document.cookie.split(";");
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		// while (c.charAt(0) == " ") c = c.substring(1, c.length);
+		while (c.charAt(0) === " ") c = c.substring(1, c.length);
+		// if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		if (c.indexOf(nameEQ) === 0)
+			return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+/* Generate a share link for the user's Mastodon domain */
+function MastodonShare(e) {
+	// Get the source text
+	src = e.target.getAttribute("data-src");
+
+	// see if a domain has been saved as a cookie
+	var cookieMastDom = readCookie("mastdom");
+	var mastdom = cookieMastDom ? cookieMastDom : "mastodon.social";
+
+	// Get the Mastodon domain
+	var domain = prompt("Enter your Mastodon domain", mastdom);
+
+	// if (domain == "" || domain == null) {
+	if (domain === "" || domain == null) {
+		e.preventDefault();
+		return false;
+	}
+
+	// save domain as a cookie
+	createCookie("mastdom", domain, 365);
+
+	// Build the URL
+	var url = `https://${domain}/share?text=${src}`;
+
+	// Open a window on the share page
+	window.open(url, "_top");
+	e.preventDefault();
+	return false;
+}
+function enableMastodonShare() {
+	var eles = document.getElementsByClassName("mastodonAskInstance");
+	for (var i = 0; i < eles.length; i++) {
+		eles[i].addEventListener("click", MastodonShare);
+	}
+}
+
+// --------------------------------------------------------------
+// Initialization
+// --------------------------------------------------------------
+window.addEventListener("load", () => {
+	// Loading content from previous pages
+	if (loopContainer?.dataset.paginatorCurrent > 1) {
+		const firstPost = document.querySelector(".js-loop__item");
+		const initialOffset = firstPost.offsetTop;
+
+		loadPosts(() => {
+			const diff = firstPost.offsetTop - initialOffset;
+
+			reloadThings();
+
+			document.documentElement.scrollTop = diff;
+		}, true);
+	}
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	enableMastodonShare();
+});
+
+loadMoreButton?.addEventListener("click", () => {
+	// this.classList.add("._spinner");
+	loadMoreButton.insertAdjacentHTML(
+		"beforeend",
+		"<div class='_spinner'></div>",
+	);
+	loadPosts(() => {
+		reloadThings();
+		if (loadMoreButton.querySelector("._spinner"))
+			loadMoreButton.querySelector("._spinner").remove();
+	});
+	debugMode && console.log("[pxl] Posts loaded");
+	// sendGAEvent("Posts", "Load more");
+	return false;
+});
+
+// document.querySelector(".js-loop__item-navigation-arrow").addEventListener("click", () => {
+//     sendGAEvent("Posts", "Post navigation");
+// });
+
 reloadThings();
 debugMode && console.log("[pxl] End of script");
